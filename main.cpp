e2bcbed91ac94d7916276cb55e4fad9e9de36e85
Anguei <42708316+Anguei@users.noreply.github.com>
2018-09-23 14:07:38 +0800

    修复 shields.io 左侧字符串显示小写的问题

--- main.cpp
+++ main.cpp
@@ -24,28 +24,28 @@ bool isNum(std::string s)
 
 void printColor(const std::string &s, const std::string &color, std::string &rgb)
 {
-	if (color == "Yellow:")
-		rgb = "f1c40f";
-	if (color == "Green:")
-		rgb = "5eb95e";
-	if (color == "Blue:")
-		rgb = "3498db";
-	if (color == "Purple:")
-		rgb = "9b59b6";
-	if (color == "Bluedark:")
-		rgb = "34495e";
-	if (color == "Gray:")
-		rgb = "bbbbbb";
-	printf("\n\n![%s](https://img.shields.io/badge/%s-%s-%s.svg)\n\n> ", s.c_str(), s.c_str(), color.substr(0, color.size() - 1).c_str(), rgb.c_str());
-	cnt = 0;
-	printf("|  |  |  |  |  |  |  |  |  |  |\n| :----------- | :----------- | :----------- | :----------- | :----------- | :----------- | :----------- | :----------- | :----------- | :----------- |\n| ");
+    if (color == "Yellow:")
+        rgb = "f1c40f";
+    if (color == "Green:")
+        rgb = "5eb95e";
+    if (color == "Blue:")
+        rgb = "3498db";
+    if (color == "Purple:")
+        rgb = "9b59b6";
+    if (color == "Bluedark:")
+        rgb = "34495e";
+    if (color == "Gray:")
+        rgb = "bbbbbb";
+    printf("\n\n![%s](https://img.shields.io/badge/%s-%s-%s.svg)\n\n> ", s.c_str(), s.c_str(), color.substr(0, color.size() - 1).c_str(), rgb.c_str());
+    cnt = 0;
+    printf("|  |  |  |  |  |  |  |  |  |  |\n| :----------- | :----------- | :----------- | :----------- | :----------- | :----------- | :----------- | :----------- | :----------- | :----------- |\n| ");
 }
 
 int main()
 {
 #ifdef yyfLocal 
-	//freopen("data.txt", "r", stdin);
-	//freopen("result.txt", "w", stdout);
+    //freopen("data.txt", "r", stdin);
+    //freopen("result.txt", "w", stdout);
     freopen("testdata.in", "r", stdin);
     freopen("testdata.out", "w", stdout);
 #endif
@@ -65,9 +65,9 @@ int main()
             preLen = 2;
         const char *prefix = subPrefix(s, preLen).c_str();
         const char *suffix = s.substr(preLen).c_str();
-		printf("[![%s](https://img.shields.io/badge/%s-%s.svg?label=%s)](https://www.luogu.org/problemnew/show/%s) $\\ $|", s.c_str(), suffix, rgb.c_str(), prefix, s.c_str());
-		++cnt;
-		if (cnt % 8 == 0) // 一行最多几个 badge
-			printf("\n| ");
+        printf("[![%s](https://img.shields.io/badge/%s-%s.svg?label=%s)](https://www.luogu.org/problemnew/show/%s) $\\ $|", s.c_str(), suffix, rgb.c_str(), prefix, s.c_str());
+        ++cnt;
+        if (cnt % 8 == 0) // 一行最多几个 badge
+            printf("\n| ");
     }
 }
